6e3d98c79cb39c0b50c50eda050bdfd04edfb3db
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-26 23:26:59 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
 import { fromEvent } from 'rxjs';
 import { buffer, filter, throttleTime } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/buffer
+// Example 1: Using buffer to recognize double clicks
+
 // streams
 const clicks$ = fromEvent(document, 'click');
 
-/*
-Collect clicks that occur, after 250ms emit array of clicks
-*/
-clicks$.pipe(
-  buffer(clicks$.pipe(throttleTime(250))),
-  // if array is greater than 1, double click occured
-  filter(clickArray => clickArray.length > 1)
-)
-.subscribe(() => console.log('Double Click!'));
+//Collect clicks that occur, after 250ms emit array of clicks
+clicks$
+  .pipe(
+    buffer(clicks$.pipe(throttleTime(250))),
+    // if array is greater than 1, double click occured
+    filter((clickArray) => clickArray.length > 1)
+  )
+  .subscribe(() => console.log('Double Click!'));
